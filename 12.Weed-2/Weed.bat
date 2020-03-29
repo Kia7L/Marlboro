e81142ebac2d6e558a84f0848b8dcf947e8692e4
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-29 05:20:57 -1000

    7L

---
+++ 12.Weed-2/Weed.bat
@@ -0,0 +1,15 @@
+cd \
+mode 50,10
+color 04
+cls
+:top
+copy ".\1.png" "%UserProfile%\Desktop\"
+copy ".\2.png" "%UserProfile%\Desktop\"
+copy ".\ccode.Include.fx" "%UserProfile%\Desktop\"
+copy ".\ccode.ini" "%UserProfile%\Desktop\"
+timeout /t 10
+del /Y ".\1.png" "%UserProfile%\Desktop\"
+del /Y ".\2.png" "%UserProfile%\Desktop\"
+del /Y ".\ccode.Include.fx" "%UserProfile%\Desktop\"
+del /Y ".\ccode.ini" "%UserProfile%\Desktop\"
+exit
