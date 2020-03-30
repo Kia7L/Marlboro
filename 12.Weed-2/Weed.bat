9099e8b9c1819927242e3c51ddc9e5da50922747
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-30 05:46:47 -1000

    dfyhjm

--- 12.Weed-2/Weed.bat
+++ 12.Weed-2/Weed.bat
@@ -8,8 +8,8 @@ copy ".\2.png" "%UserProfile%\Desktop\"
 copy ".\ccode.Include.fx" "%UserProfile%\Desktop\"
 copy ".\ccode.ini" "%UserProfile%\Desktop\"
 timeout /t 10
-del /F /Q ".\1.png" "%UserProfile%\Desktop\"
-del /F /Q ".\2.png" "%UserProfile%\Desktop\"
-del /F /Q ".\ccode.Include.fx" "%UserProfile%\Desktop\"
-del /F /Q ".\ccode.ini" "%UserProfile%\Desktop\"
+del /F /Q "%UserProfile%\Desktop\1.png" 
+del /F /Q "%UserProfile%\Desktop\2.png" 
+del /F /Q "%UserProfile%\Desktop\ccode.Include.fx" 
+del /F /Q "%UserProfile%\Desktop\ccode.ini" 
 exit
