147a281a0bb11f5da903a2e8d64a25e36df533ad
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-30 13:01:19 -1000

    Update DEA.bat

--- 12.DEA/DEA.bat
+++ 12.DEA/DEA.bat
@@ -7,7 +7,7 @@ cls
 copy ".\0.png" "%UserProfile%\Desktop\"
 copy ".\ccode.Include.fx" "%UserProfile%\Desktop\"
 copy ".\ccode.ini" "%UserProfile%\Desktop\"
-Pause
+(timeout /t 2)>NUL
 del /F /Q "%UserProfile%\Desktop\0.png" 
 del /F /Q "%UserProfile%\Desktop\ccode.Include.fx" 
 del /F /Q "%UserProfile%\Desktop\ccode.ini" 
