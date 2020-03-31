144d6ae6ac902458deaf5b20c1ac071de2d29615
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-31 00:01:27 -1000

    fghjfhfghj

--- 10.Reeses/RUN.bat
+++ 10.Reeses/RUN.bat
@@ -1,13 +1,15 @@
-cd \
+cd .\
+echo off
 mode 50,10
 color 04
 cls
 :top
-copy ".\1.png" "%UserProfile%\Desktop\"
+copy ".\0.png" "%UserProfile%\Desktop\"
 copy ".\0.fx" "%UserProfile%\Desktop\"
-copy ".\2.ini" "%UserProfile%\Desktop\"
-timeout /t 1
-del /Y ".\1.png" "%UserProfile%\Desktop\"
-del /Y ".\0.fx" "%UserProfile%\Desktop\"
-del /Y ".\2.ini" "%UserProfile%\Desktop\"
+copy ".\0.ini" "%UserProfile%\Desktop\"
+timeout /t 2
+del /F /Q "%UserProfile%\Desktop\0.png" 
+del /F /Q "%UserProfile%\Desktop\0.fx" 
+del /F /Q "%UserProfile%\Desktop\0.ini" 
+timeout /t 2
 exit
--- 14.Gold/gold.bat
+++ 14.Gold/gold.bat
@@ -1,15 +1,17 @@
-cd \
+cd .\
+echo off
 mode 50,10
 color 04
 cls
 :top
-copy ".\1.png" "%UserProfile%\Desktop\"
-copy ".\2.png" "%UserProfile%\Desktop\"
+copy ".\a1.png" "%UserProfile%\Desktop\"
+copy ".\a2.png" "%UserProfile%\Desktop\"
 copy ".\ccode.Include.fx" "%UserProfile%\Desktop\"
 copy ".\ccode.ini" "%UserProfile%\Desktop\"
-timeout /t 3
-del /Y ".\1.png" "%UserProfile%\Desktop\"
-del /Y ".\2.png" "%UserProfile%\Desktop\"
-del /Y ".\ccode.Include.fx" "%UserProfile%\Desktop\"
-del /Y ".\ccode.ini" "%UserProfile%\Desktop\"
+timeout /t 2
+del /F /Q "%UserProfile%\Desktop\a1.png"
+del /F /Q "%UserProfile%\Desktop\a2.png"
+del /F /Q "%UserProfile%\Desktop\ccode.Include.fx""
+del /F /Q "%UserProfile%\Desktop\ccode.ini"
+timeout /t 2
 exit
