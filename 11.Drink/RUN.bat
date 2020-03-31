6599d891928e057287cf62365c624c6f1af8fd54
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-31 00:02:33 -1000

    cghj

--- 11.Drink/RUN.bat
+++ 11.Drink/RUN.bat
@@ -1,5 +1,5 @@
 cd .\
-@echo off
+echo off
 mode 50,10
 color 04
 cls
@@ -7,9 +7,9 @@ cls
 copy ".\0.png" "%UserProfile%\Desktop\"
 copy ".\0.fx" "%UserProfile%\Desktop\"
 copy ".\0.ini" "%UserProfile%\Desktop\"
-(timeout /t 2)>NUL
-del /F Q "%UserProfile%\Desktop\0.png" "
-del /F Q "%UserProfile%\Desktop\0.fx" 
-del /F Q "%UserProfile%\Desktop\0.ini" 
-(timeout /t 2)>NUL
+timeout /t 2
+del /F /Q "%UserProfile%\Desktop\0.png" 
+del /F /Q "%UserProfile%\Desktop\0.fx" 
+del /F /Q "%UserProfile%\Desktop\0.ini" 
+timeout /t 2
 exit
