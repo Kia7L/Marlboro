bf5db8728cbce96b9ad058f6c0269c2cb1d8f21c
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-31 00:55:37 -1000

    7L

---
+++ 147L.Mortorcycle/0.fx
@@ -0,0 +1,5 @@
+AliasAssist=0 1110 ;Layers Off=0 ON=1
+AliasAssist2=0 1111 ;Layers Off=0 ON=1
+(@*<7L000.ini>) AliasAssist2
+;;Kia7L Made This Not Pulp White Garbage...
+
---
+++ 147L.Mortorcycle/RUN.bat
@@ -0,0 +1,38 @@
+cd .\
+echo off
+mode 50,10
+color 04
+cls
+:top
+nircmd speak text "tilt,monitor,toward,the,ground,ten,to,eleven,degrees"
+nircmd speak text "stand,to,the,side,clear,the,area,three,seconds"
+
+(ping -n 1 192.168.1.1)>NUL
+nircmd speak text "3"
+cecho {09}3{\n}
+(ping -n 1 192.168.1.1)>NUL
+nircmd speak text "2"
+cecho {09}2{\n}
+(ping -n 1 192.168.1.1)>NUL
+nircmd speak text "1"
+@color 40
+cecho {40}1{\n}
+
+copy ".\1.png" "%UserProfile%\Desktop\"
+copy ".\0.fx" "%UserProfile%\Desktop\"
+copy ".\0.ini" "%UserProfile%\Desktop\"
+timeout /t 2
+del /F /Q "%UserProfile%\Desktop\1.png" 
+del /F /Q "%UserProfile%\Desktop\0.fx" 
+del /F /Q "%UserProfile%\Desktop\0.ini" 
+timeout /t 2
+
+copy ".\2.png" "%UserProfile%\Desktop\"
+copy ".\0.fx" "%UserProfile%\Desktop\"
+copy ".\0.ini" "%UserProfile%\Desktop\"
+timeout /t 2
+del /F /Q "%UserProfile%\Desktop\2.png" 
+del /F /Q "%UserProfile%\Desktop\0.fx" 
+del /F /Q "%UserProfile%\Desktop\0.ini" 
+timeout /t 2
+exit
