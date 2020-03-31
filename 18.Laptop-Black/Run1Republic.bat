946f7bd9c422f99a245a773ff686bf612c2b5db9
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-31 01:57:09 -1000

    7L

---
+++ 18.Laptop-Black/Run1Republic.bat
@@ -0,0 +1,15 @@
+cd .\
+@Echo off
+mode 50,10
+color 04
+cls
+:top
+copy ".\0.png" "%UserProfile%\Desktop\"
+copy ".\ccode.Include.fx" "%UserProfile%\Desktop\"
+copy ".\ccode.ini" "%UserProfile%\Desktop\"
+(timeout /t 2)>NUL
+del /F /Q "%UserProfile%\Desktop\0.png" 
+del /F /Q "%UserProfile%\Desktop\ccode.Include.fx" 
+del /F /Q "%UserProfile%\Desktop\ccode.ini" 
+(timeout /t 2)>NUL
+exit
